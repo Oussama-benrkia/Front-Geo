c0a5475b74d79e2542f66ef09df4272a168ac216
L-Yes01 <ilyass.ham16@gmail.com>
2024-05-24 15:40:54 +0100

    Update add-vehicle.js

--- pages/add-vehicle.js
+++ pages/add-vehicle.js
@@ -20,7 +20,7 @@ function AddVehicle() {
       }
       return token;
     };
-    let a=checkAuthentication()
+    let a = checkAuthentication()
   })
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -34,7 +34,7 @@ function AddVehicle() {
     if (image) {
       formData.append('image', image);
     }
-    
+
     const checkAuthentication = () => {
       const token = localStorage.getItem('token');
       if (!token) {
@@ -42,7 +42,7 @@ function AddVehicle() {
       }
       return token;
     };
-    const token=checkAuthentication()
+    const token = checkAuthentication()
     try {
       const response = await fetch('http://localhost:8081/api/vehicule', {
         method: 'POST',
@@ -56,8 +56,8 @@ function AddVehicle() {
         throw new Error('Failed to add vehicle');
       }
 
-      console.log('Vehicle added successfully');
-      router.push('/'); // Navigate back to the main page or vehicle list
+      alert('Vehicle added successfully');
+      router.push('/components/forms'); // Navigate back to the main page or vehicle list
     } catch (error) {
       console.error('Error adding vehicle:', error);
     }
@@ -70,45 +70,58 @@ function AddVehicle() {
   return (
     <div className={styles.formContainer}>
       <h1 className={styles.title}>Add New Vehicle</h1>
-      <form onSubmit={handleSubmit}className={styles.form}>
+      <form onSubmit={handleSubmit} className={styles.form}>
         <div className={styles.formGroup}>
           <label>Matricule:</label>
-          <input             className={styles.input}
- type="text" value={matricule} onChange={(e) => setMatricule(e.target.value)} required />
+          <input className={styles.input}
+            type="text" value={matricule} onChange={(e) => setMatricule(e.target.value)} required />
         </div>
         <div className={styles.formGroup}>
           <label>Nom:</label>
-          <input             className={styles.input}
- type="text" value={nom} onChange={(e) => setNom(e.target.value)} required />
+          <input className={styles.input}
+            type="text" value={nom} onChange={(e) => setNom(e.target.value)} required />
         </div>
         <div className={styles.formGroup}>
           <label>Modele:</label>
-          <input             className={styles.input}
- type="text" value={modele} onChange={(e) => setModele(e.target.value)} required />
+          <input className={styles.input}
+            type="text" value={modele} onChange={(e) => setModele(e.target.value)} required />
         </div>
-        
+
         <div className={styles.formGroup}>
           <label>Etat:</label>
-          <select             className={styles.input}
- value={etat} onChange={(e) => setEtat(e.target.value)}>
+          <select className={styles.input}
+            value={etat} onChange={(e) => setEtat(e.target.value)}>
             <option value="actif">Actif</option>
             <option value="inactif">Inactif</option>
           </select>
         </div>
-            <div className={styles.formGroup}>
+        <div className={styles.formGroup}>
           <label>Status:</label>
-          <input             className={styles.input}
- type="text" value={status} onChange={(e) => setStatus(e.target.value)} required />
+          <select className={styles.input}
+            value={status} onChange={(e) => setStatus(e.target.value)} required >
+            <option value="En_location">En_location</option>
+            <option value="En_Panne">En_Panne</option>
+            <option value="En_cours_de_réparation">En_cours_de_réparation</option>
+            <option value="En_new">En_new</option>
+          </select>
         </div>
         <div className={styles.formGroup}>
           <label>Type:</label>
-          <input             className={styles.input}
- type="text" value={type} onChange={(e) => setType(e.target.value)} required />
+          <select className={styles.input}
+            value={type} onChange={(e) => setType(e.target.value)} required>
+            <option value="A">A</option>
+            <option value="B">B</option>
+            <option value="C">C</option>
+            <option value="D">D</option>
+            <option value="EB">EB</option>
+            <option value="EC">EC</option>
+            <option value="ED">ED</option>
+          </select>
         </div>
         <div className={styles.formGroup}>
           <label>Image:</label>
-          <input             className={styles.input}
- type="file" onChange={handleImageChange} />
+          <input className={styles.input}
+            type="file" onChange={handleImageChange} />
         </div>
         <button type="submit" className={styles.submitButton}>Add Vehicle</button>
       </form>
